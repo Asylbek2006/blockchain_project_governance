af08d42fc2e4ee9918a2be7358d87a3b430c498a
aabdibay <asylbekabdibay@gmail.com>
2026-05-01 15:21:07 +0500

    fix

--- src/GovernanceToken.sol
+++ src/GovernanceToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "openzeppelin-contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "openzeppelin-contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "openzeppelin-contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
     uint256 public constant TOTAL_SUPPLY = 1_000_000 *10 ** 18;
